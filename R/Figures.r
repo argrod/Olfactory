393e21312f14933a5102f27b27c4df670ff2acd7
argrod <aran.garrod@gmail.com>
2021-04-27 23:04:10 +0900

    Added a figure output generation file

---
+++ R/Figures.r
@@ -0,0 +1,176 @@
+library(rerddap)
+library(splitr)
+library(dplyr)
+library(plyr)
+library(ggplot2)
+library(rnaturalearth)
+library(sp)
+# devtools::install_github('ropensci/plotdap')
+library(plotdap)
+library(scales)
+# install.packages("StreamMetabolism",repos='http://cran.us.r-project.org')
+library(poweRlaw)
+library(ggpubr)
+library(maptools)
+library(StreamMetabolism)
+library(RColorBrewer)
+library(reshape)
+library(circular)
+library(wGribDat)
+# devtools::install_github("keesmulder/circglmbayes")
+library(circglmbayes)
+library(lme4)
+library(ncdf4)
+# install.packages("CircStats")
+library(CircStats)
+library(Cairo)
+options(timeout = 800)
+
+if(Sys.info()['sysname'] == "Darwin"){
+    load("/Volumes/GoogleDrive/My Drive/PhD/Data/2019Shearwater/2019Dat.RData")
+    load("/Volumes/GoogleDrive/My Drive/PhD/Data/Temp2018.RData")
+} else {
+    load("F:/UTokyoDrive/PhD/Data/2019Shearwater/2019Dat.RData")
+    load("F:/UTokyoDrive/PhD/Data/Temp2018.RData")
+}
+D18 <- bind_rows(Dat)
+D19 <- bind_rows(Dat19)
+allD <- data.frame(DT=c(D18$DT, D19$DT),
+    lat = c(D18$Lat, D19$Lat),
+    lon = c(D18$Lon, D19$Lon),
+    tagID = c(D18$tagID, D19$tagID),
+    Day = c(D18$Day, D19$Day),
+    Sex = c(D18$Sex, D19$Sex),
+    distTrav = c(D18$recalDist, D19$recalDist),
+    spTrav = c(D18$spTrav, D19$spTrav),
+    recalSp = c(D18$recalSp, D19$recalSp),
+    distFk = c(D18$distFromFk, D19$distFromFk),
+    tripL = c(D18$tripL, D19$tripL),
+    tkb = c(D18$tkb, D19$tkb),
+    dv = c(D18$dv, D19$dv),
+    UTME = c(D18$UTME, D19$UTME),
+    UTMN = c(D18$UTMN, D19$UTMN))
+allD$Year <- format(allD$DT, format = "%Y")
+allD$forage <- allD$dv == 1 | allD$tkb == 1
+japan <- ne_countries(scale = "medium", country = "Japan", returnclass = "sf")
+# FORAGING SPOT DISPERSALS
+if(Sys.info()['sysname'] == "Darwin"){
+    load("/Volumes/GoogleDrive/My Drive/PhD/Data/splitr/ForageDisps.RData")
+} else {
+    load("F:/UTokyoDrive/PhD/Data/splitr/ForageDisps.RData")
+}
+allD$forage[is.na(allD$forage)] <- 0
+forSt <- which(diff(allD$forage) == 1) + 1
+if(allD$forage[1] == 1){
+  forSt <- c(1, forSt)
+}
+forEd <- which(diff(allD$forage) == -1)
+if(allD$forage[nrow(allD)] == 1){
+  forEd <- c(forEd, nrow(allD))
+}
+# LOAD IN THE STEP LENGTHS TRAJECTORIES
+if(Sys.info()['sysname'] == "Darwin"){
+    load("/Volumes/GoogleDrive/My Drive/PhD/Data/splitr/StepsTrajTimeChg.RData")
+} else {
+    load("F:/UTokyoDrive/PhD/Data/splitr/StepsTrajTimeChg.RData")
+}
+# save(ListD, file="/Volumes/GoogleDrive/My Drive/PhD/Data/splitr/ListD.RData")
+# LOAD IN THE LISTED DATA
+if(Sys.info()['sysname'] == "Darwin"){
+    load("/Volumes/GoogleDrive/My Drive/PhD/Data/splitr/ListD.RData")
+} else {
+    load("F:/UTokyoDrive/PhD/Data/splitr/ListD.RData")
+}
+# LOAD WIND DATA
+if(Sys.info()['sysname'] == "Darwin"){
+  load("/Volumes/GoogleDrive/My Drive/PhD/Data/WindCalc/windDatAll.RData")
+} else {
+  load("F:/UTokyoDrive/PhD/Data/WindCalc/windDatAll.RData")
+}
+allTraj <- bind_rows(outTraj)
+allTraj$relH <- allTraj$aveHd - allTraj$trjHd
+
+# figure locations
+if(Sys.info()['sysname'] == "Darwin"){
+  figLoc <- "/Volumes/GoogleDrive/My Drive/PhD/Figures/Olfactory/"
+} else {
+  figLoc <- "F:/UTokyoDrive/PhD/Figures/Olfactory/"
+}
+
+# LESS THAN 10KM
+ggplot(WindDat[WindDat$distTo < 10,]) + 
+    geom_histogram(aes(x = RelHead), colour = "black", bins = 50) + 
+    geom_vline(xintercept = circ.mean(WindDat$RelHead[WindDat$distTo < 10]), size = 1.3, linetype = 1) +
+    coord_polar(start=pi) + scale_x_continuous(name = "Relative wind heading", breaks = c(pi,-pi/2,0,pi/2), labels = c("-180/180","-90","0","90"), limits = c(-pi,pi)) + 
+    theme_bw() + theme(panel.grid.minor = element_blank()) + theme(panel.border = element_rect(colour = 'black', fill = NA), text = element_text(size = 12,
+        family = "Arial"), axis.text = element_text(size = 12, family = "Arial"))
+
+one2Ten <- vector(mode="list",length=5)
+distGaps <- seq(0,8,2)
+distGapsL <- distGaps+2
+for(b in 1:length(distGaps)){
+    RaylT <- r.test(WindDat$RelHead[WindDat$distTo >= distGaps[b] & WindDat$distTo < distGapsL[b]])
+    if(RaylT$p.value > 0.05){
+        one2Ten[[b]] <- ggplot(WindDat[WindDat$distTo >= distGaps[b] & WindDat$distTo < distGapsL[b],]) + 
+            geom_histogram(aes(x = RelHead), colour = "black", bins = 50) + scale_y_continuous(name = "Count") +
+            coord_polar(start=pi) + scale_x_continuous(name = "Relative wind heading", breaks = c(pi,-pi/2,0,pi/2), labels = c("-180/180","-90","0","90"), limits = c(-pi,pi)) + 
+            theme_bw() + theme(panel.grid.minor = element_blank()) + theme(panel.border = element_rect(colour = 'black', fill = NA), text = element_text(size = 10,
+                family = "Arial"), axis.text = element_text(size = 8, family = "Arial"))
+    } else {
+        roseplt <- ggplot(WindDat[WindDat$distTo >= distGaps[b] & WindDat$distTo < distGapsL[b],]) + 
+            geom_histogram(aes(x = RelHead), colour = "black", bins = 50) + scale_y_continuous(name = "Count") +
+            geom_vline(xintercept = circ.mean(WindDat$RelHead[WindDat$distTo >= distGaps[b] & WindDat$distTo < distGapsL[b]]), size = 1.3, linetype = 1) +
+            coord_polar(start=pi) + scale_x_continuous(name = "Relative wind heading", breaks = c(pi,-pi/2,0,pi/2), labels = c("-180/180","-90","0","90"), limits = c(-pi,pi)) + 
+            theme_bw() + theme(panel.grid.minor = element_blank()) + theme(panel.border = element_rect(colour = 'black', fill = NA), text = element_text(size = 10,
+                family = "Arial"), axis.text = element_text(size = 8, family = "Arial")) 
+        one2Ten[[b]] <- roseplt + geom_label(aes(x = pi/4, y = max(ggplot_build(roseplt)$data[[1]]$count)), label = paste("p value = ",as.character(signif(RaylT$p.value, 3)), sep = ""))
+    }
+    Cairo(width=8, height = 8, file = paste(figLoc,"RelHead",as.character(distGaps[b]),"-",as.character(distGapsL[b]),".svg",sep=""),type="svg", bg = "transparent", dpi = 300, units="in")
+    one2Ten[[b]]
+    dev.off()
+}
+ggarrange(one2Ten[[1]],one2Ten[[2]],one2Ten[[3]],one2Ten[[4]],one2Ten[[5]], nrow=2, ncol=3, labels=c("0-2km","2-4km","4-6km","6-8km","8-10km"))
+circ.mean(WindDat$RelHead[WindDat$distTo < 10])
+rose.diag(WindDat$RelHead[WindDat$distTo < 10],50,prop=3)
+r.test(WindDat$RelHead[WindDat$distTo < 10])
+r.test(WindDat$RelHead[WindDat$distTo < 1])
+
+circ.mean(WindDat$RelHead[WindDat$distTo >= 10 & WindDat$distTo < 20])
+r.test(WindDat$RelHead[WindDat$distTo >= 10 & WindDat$distTo < 20])
+circ.mean(WindDat$RelHead[WindDat$distTo >= 20 & WindDat$distTo < 30])
+circ.mean(WindDat$RelHead[WindDat$distTo >= 30 & WindDat$distTo < 40])
+
+circ.mean(na.omit(allTraj$relH[allTraj$distTo < 10*10^3]))
+circ.mean(na.omit(allTraj$relH[allTraj$distTo < 10*10^3]))
+r.test(na.omit(allTraj$relH[allTraj$distTo < 10*10^3]))
+
+allTraj$relH[allTraj$relH < -pi] <- allTraj$relH[allTraj$relH < -pi] + 2*pi
+allTraj$relH[allTraj$relH > pi] <- allTraj$relH[allTraj$relH > pi] - 2*pi
+ggplot(allTraj[allTraj$distTo < 10*10^3,], aes(x = relH, y = distTo)) + 
+ geom_point() + coord_polar()
+
+lengths<-seq(from=0,to=max(WindDat$distTo)-1, by = 1)
+lengthsL <- lengths+1 
+binDat <- data.frame(dist = lengthsL, aveHd = unlist(lapply(1:length(lengths), function(x) circ.mean(WindDat$RelHead[WindDat$distTo >= lengths[x] & WindDat$distTo < lengthsL[x]]))),
+  disp = unlist(lapply(1:length(lengths), function(x) circ.disp(WindDat$RelHead[WindDat$distTo >= lengths[x] & WindDat$distTo < lengthsL[x]])$var)))
+
+binDat <- binDat[!is.na(binDat$disp),]
+Cairo(width=8, height = 8, file = paste(figLoc,"DispersalOverDist.svg",sep=""),type="svg", bg = "transparent", dpi = 300, units="in")
+ggplot(binDat[binDat$dist < 150,], aes(x = dist, y = disp)) + geom_line() + geom_vline(xintercept=10, linetype="dotted") +
+    theme_bw() + theme(panel.grid = element_blank()) + theme(panel.border = element_rect(colour = 'black', fill = NA), text = element_text(size = 10,
+            family = "Arial"), axis.text = element_text(size = 8, family = "Arial")) + scale_x_continuous(name="Distance to next foraging (km)") +
+    scale_y_continuous(name = "Angular dispersal")
+dev.off()
+
+breaks<-seq(from=0,to=round_any(max(WindDat$distTo),10,f=ceiling),by=10)
+mnW <- ddply(WindDat, "bin10", summarise, grp.mean=mean(aligned))
+WindDat$bin10 <- cut(WindDat$distTo, breaks = breaks, include.lowest=T,right=F)
+Cairo(width=8, height = 8, file = paste(figLoc,"DistRelDensity.svg",sep=""),type="svg", bg = "transparent", dpi = 300, units="in")
+ggplot(WindDat[WindDat$distTo > 0 &WindDat$distTo < 90,], aes(x = aligned, colour = bin10)) +#max(WindDat$distTo),], aes(x = aligned, colour = bin10)) +
+  # geom_histogram(alpha=.2,fill=NA,position="dodge")
+  geom_density(alpha=.2,show.legend=FALSE)+stat_density(aes(x=aligned, colour=bin10), geom="line",position="identity") +
+  scale_x_continuous(name = "Relative wind heading", breaks=c(-pi, -pi/2, 0, pi/2, pi), labels=c("-180","-90","0","90","180")) + ylab("Density") + theme_bw() + theme(panel.grid = element_blank()) +
+  theme(panel.border = element_rect(colour = 'black', fill = NA), text = element_text(size = 10,
+        family = "Arial"), axis.text = element_text(size = 8, family = "Arial")) + 
+  scale_colour_manual(name="Distance to foraging (km)", values = rev(brewer.pal(9,"YlOrRd")))
+dev.off()
