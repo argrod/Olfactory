7f31896a1d1ae988e22ceb42ebea4f7ede1940cc
argrod <aran.garrod@gmail.com>
2021-08-20 01:20:11 +0900

    Created distribution method diagrams

--- Figures.r
+++ Figures.r
@@ -1,4 +1,20 @@
-	
+library(ggplot2)
+library(ggforce)
+library(extrafont)
+
+if(Sys.info()['sysname'] == "Darwin"){
+  figLoc <- "/Volumes/GoogleDrive/My Drive/PhD/Figures/Olfactory/"
+  # figLoc <- "/Documents/GitHub/PhD/Olfactory/"
+} else {
+  figLoc <- "G:/UTokyoDrive/PhD/Figures/Olfactory/"
+  # figLoc <- "F:/Documents/GitHub/PhD/Olfactory/"
+}
+
+# run MinuteHeading_WindEstimation with these variables
+a = 1
+center = 1149
+# which(as.character(tp) == "2018-09-03 18:33:34")
+
     yoko<-Von_Mises_sd(sqrt(ans_best$par[3]*ans_best$par[3]+ans_best$par[2]*ans_best$par[2]))*Weibull_mean(ans_best$par[1],constv/gamma(1+1/ans_best$par[1]))
 #    yoko: SD of the heading vector distribution to the
 #perpendicular direction relative to the mean direction	
@@ -21,109 +37,50 @@ xComp <- rrow[st:end] * cos(drow[st:end])
 yComp <- rrow[st:end] * sin(drow[st:end])
 
 
+
 plot(mean(xComp),mean(yComp))
 ggplot() + 
+    geom_circle(aes(x0 = 0, y0 = 0, r = sqrt((mean(xComp) + ans_best$par[4])^2 + (mean(yComp) + ans_best$par[5])^2)),size = 1.5,colour="#225ea8") +
+    theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+      text = element_text(size = 10),axis.text = element_text(size = 8)) +
+    scale_x_continuous(name = expression(paste("U component (m", s^{-1},")"))) +
+    scale_y_continuous(name = expression(paste("V component (m", s^{-1},")"))) +
+    geom_circle(aes(x0 = 0, y0 = 0, r = sqrt(mean(xComp)^2 + mean(yComp)^2)),size = 1.5,colour="#238443") +
     geom_segment(aes(x = 0, y = 0, xend = mean(xComp), yend = mean(yComp)),
-    arrow = arrow(length = unit(0.03, "npc"))) +
-    geom_segment(aes(xend = mean(xComp), yend = mean(yComp), x = mean(xComp) + ans_best$par[4], y = mean(yComp) + ans_best$par[5]),
-    arrow = arrow(length = unit(0.03, "npc")), colour = "blue") +
+    arrow = arrow(length = unit(0.03, "npc"),type="closed"),size=1.1,linejoin="round") +
+    geom_segment(aes(x = mean(xComp), y = mean(yComp), xend = mean(xComp) + ans_best$par[4], yend = mean(yComp) + ans_best$par[5]),
+    arrow = arrow(length = unit(0.03, "npc"),type="closed"), colour = "blue",size=1,linejoin="round") +
     geom_segment(aes(x = 0, y = 0, xend = mean(xComp) + ans_best$par[4], yend = mean(yComp) + ans_best$par[5]),
-    arrow = arrow(length = unit(0.03, "npc")), colour = "red")
+    arrow = arrow(length = unit(0.03, "npc"),type="closed"), colour= "#ef3b2c",size=1,linejoin="round")
 
-library(ggplot2)
-ggplot() + geom_path(aes(x = long[st:end], y = lat[st:end]))
+ggsave(paste(figLoc,"DistMethod.svg",sep=""), device="svg", dpi = 300, height = 2,
+      width = 2, units = "in")
+dev.off()
+
+ggplot() + geom_path(aes(x = long[st:end], y = lat[st:end])) +
+  theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        text = element_text(size = 10),axis.text = element_text(size = 8)) +
+  scale_x_continuous(name = expression(paste("Longitude ("*degree*")")), breaks = c(142.55,142.600)) + 
+  scale_y_continuous(name = expression(paste("Latitude ("*degree*")")),breaks = c(41.0,41.1,41.2)) + 
+  geom_segment(aes(x=long[seq(st,end-1,round((end-st-1)/5))],
+  y = lat[seq(st,end,round((end-st-1)/5))],
+  xend = long[seq(st+1,end,round((end-st+1)/5))],
+  yend = lat[seq(st+1,end,round((end-st+1)/5))]),
+  arrow = arrow(length = unit(0.1,"inches")))
+
+ggsave(paste(figLoc,"DistSegment.svg",sep=""), device="svg", dpi = 300, height = 2,
+      width = 2, units = "in")
+dev.off()
 
 ggplot() + geom_point(aes(x = d, y = r)) + 
     scale_x_continuous(limits=c(-pi,pi))
 
-ggplot() + geom_point(aes(x = drow[st:end], y = rrow[st:end]))
-inithd_first
+ggplot() + geom_point(aes(x = drow[st:end], y = rrow[st:end])) +
+  theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        text = element_text(size = 10),axis.text = element_text(size = 8)) +
+  scale_x_continuous(name = expression(paste("Headings (rad)"))) + 
+  scale_y_continuous(name = expression(paste("Speed (m"*s^{-1}*")")))
 
-
-## m=matrix(data=sample(rnorm(100,mean=0,sd=2)), ncol=10)
-## this function makes a graphically appealing heatmap (no dendrogram) using ggplot
-## whilst it contains fewer options than gplots::heatmap.2 I prefer its style and flexibility
- 
-ggheat=function(m, rescaling='none', clustering='none', labCol=T, labRow=T, border=FALSE, 
-heatscale= c(low='blue',high='red'))
-{
-  ## the function can be be viewed as a two step process
-  ## 1. using the rehape package and other funcs the data is clustered, scaled, and reshaped
-  ## using simple options or by a user supplied function
-  ## 2. with the now resahped data the plot, the chosen labels and plot style are built
- 
-  require(reshape)
-  require(ggplot2)
- 
-  ## you can either scale by row or column not both! 
-  ## if you wish to scale by both or use a differen scale method then simply supply a scale
-  ## function instead NB scale is a base funct
- 
-  if(is.function(rescaling))
-  { 
-    m=rescaling(m)
-  } 
-  else 
-  {
-    if(rescaling=='column') 
-      m=scale(m, center=T)
-    if(rescaling=='row') 
-      m=t(scale(t(m),center=T))
-  }
- 
-  ## I have supplied the default cluster and euclidean distance- and chose to cluster after scaling
-  ## if you want a different distance/cluster method-- or to cluster and then scale
-  ## then you can supply a custom function 
- 
-  if(is.function(clustering)) 
-  {
-    m=clustering(m)
-  }else
-  {
-  if(clustering=='row')
-    m=m[hclust(dist(m))$order, ]
-  if(clustering=='column')  
-    m=m[,hclust(dist(t(m)))$order]
-  if(clustering=='both')
-    m=m[hclust(dist(m))$order ,hclust(dist(t(m)))$order]
-  }
-    ## this is just reshaping into a ggplot format matrix and making a ggplot layer
- 
-  rows=dim(m)[1]
-  cols=dim(m)[2]
-  melt.m=cbind(rowInd=rep(1:rows, times=cols), colInd=rep(1:cols, each=rows) ,melt(m))
-    g=ggplot(data=melt.m)
- 
-  ## add the heat tiles with or without a white border for clarity
- 
-  if(border==TRUE)
-    g2=g+geom_rect(aes(xmin=colInd-1,xmax=colInd,ymin=rowInd-1,ymax=rowInd, fill=value),colour='white')
-  if(border==FALSE)
-    g2=g+geom_rect(aes(xmin=colInd-1,xmax=colInd,ymin=rowInd-1,ymax=rowInd, fill=value))
- 
-  ## add axis labels either supplied or from the colnames rownames of the matrix
- 
-  if(labCol==T) 
-    g2=g2+scale_x_continuous(breaks=(1:cols)-0.5, labels=colnames(m))
-  if(labCol==F) 
-    g2=g2+scale_x_continuous(breaks=(1:cols)-0.5, labels=rep('',cols))
- 
-  if(labRow==T) 
-    g2=g2+scale_y_continuous(breaks=(1:rows)-0.5, labels=rownames(m))    
-    if(labRow==F) 
-    g2=g2+scale_y_continuous(breaks=(1:rows)-0.5, labels=rep('',rows))    
- 
-  ## get rid of grey panel background and gridlines
- 
-  g2=g2+opts(panel.grid.minor=theme_line(colour=NA), panel.grid.major=theme_line(colour=NA),
-  panel.background=theme_rect(fill=NA, colour=NA))
- 
-  ## finally add the fill colour ramp of your choice (default is blue to red)-- and return
-  return(g2+scale_fill_continuous("", heatscale[1], heatscale[2]))
- 
-}
- 
-  ## NB because ggheat returns an ordinary ggplot you can add ggplot tweaks post-production e.g. 
-  ## data(mtcars)
-  ## x= as.matrix(mtcars)
-  ## ggheat(x, clustCol=T)+ opts(panel.background=theme_rect(fill='pink'))
+ggsave(paste(figLoc,"DistSpdHd.svg",sep=""), device="svg", dpi = 300, height = 2,
+      width = 2, units = "in")
+dev.off()
