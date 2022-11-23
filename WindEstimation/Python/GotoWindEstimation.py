Fixed the likelihood calculation. fnscale in R's optim performs fn(par)/fnscale, not fnscale/fn(par). Don't want to talk about it

WindEstimation/Python/GotoWindEstimation.py
@@ -1,7 +1,6 @@
 import scipy as sp
 import numpy as np
 import pandas as pd
-import utm
 import csv
 from statistics import mode
 import torch
@@ -9,11 +8,24 @@ import os, re, glob, pyproj, math, datetime
 from sys import platform
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
+import geopandas as gpd
 
 timeWindow = 51
 cutlength = 45
 cutv = 4.1667
 constv = 34.7/3.6
+
+points= gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(x=df.lon, y=df.lat), crs = 'EPSG:4326')
+df
+
+rrow = [6.502933,7.621181,8.563742,6.304837,9.630127,9.503375,5.990518,10.615128,8.977472,11.682448,9.937076,8.986239,11.529867,12.037547,11.027331,11.958726,11.674352,9.236950,11.482676,11.099358,10.216385,10.680228,11.922089,11.138566,12.012096,10.430966,10.898318,11.373726,10.612603,12.276527,12.064162,9.506190,10.835805,10.837890,11.434897,11.542841,11.880499,11.840336,11.322806,10.209236,7.227563,11.507512,11.538174,10.721647,10.499892,9.892160]
+drow = [1.3566603,1.0684093,1.0587566,0.9347287,1.2419793,1.2388479,1.0015028,1.4442361,1.8459093,1.9182309,1.6511230,1.5017697,1.5017876,1.5411053,1.5031676,1.5000700,1.3827510,1.1922057,1.3152792,1.2855452,1.1636894,1.3092694,1.2440532,1.2413781,1.2151118,1.0770955,1.2063922,1.1382877,1.0852326,1.0732983,1.1655605,1.2568569,1.0391123,0.9735457,0.9969128,1.1527833,1.2271071,1.1667219,1.0243778,1.0152556,0.8155524,1.2601247,1.2242784,1.0676499,1.0946004,1.0344830]
+
+myProj = pyproj.Proj("+proj=utm +zone=54, +north +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
+
+plt.scatter(np.diff(X[0:1000])**2,np.diff(RDat.X[0:1000])**2)
+plt.show()
+plt.plot(spd*3.6)
 
 def Likelihoodww(data1: np.ndarray,data2: np.ndarray,cv: np.ndarray): # calculate log-likelihood of the model
     def f(par):
@@ -25,7 +37,7 @@ def Likelihoodww(data1: np.ndarray,data2: np.ndarray,cv: np.ndarray): # calculat
             ry = (i*np.sin(j)-par[4])/r1
             lp = (par[0]-2) * math.log(r1) - (r1/b)**par[0] + par[1]*rx + par[2]*ry + math.log(par[0]) - math.log(b) + (1-par[0])*math.log(b) - math.log(sp.special.iv(0,np.sqrt(pow(par[1],2) + pow(par[2],2)),))
             L = L+lp
-        return -1/L
+        return L/-1
     return f
 for i,j in zip(df.track_speed[windows[x]],df.track_direction[windows[x]]):
     r1 = np.sqrt(pow((i*np.cos(j) - tester[3]),2) + pow((i*np.sin(j) - tester[4]),2))
@@ -59,10 +71,10 @@ def nearest(items, pivot): # find the nearest time position
 
 def timeRescale(dat,tdiff): # calculated indeces for rescaling time (DT) for regular sampling every tdiff mins
     return dat.iloc[np.arange(0,len(dat),step=np.timedelta64(tdiff,'m')/np.timedelta64(mode(np.diff(dat['DT'])),'s')).astype(int),:].reset_index()
-np.timedelta64(np.diff(df.DT),'m')
+
 def distLatLon(lat,lon):
-    X,Y,_,_ = utm.from_latlon(np.array(lat),np.array(lon))
-    return np.append(np.nan,np.sqrt((np.diff(X))**2 + (np.diff(Y))**2))
+    X,Y,_,_ = utm.from_latlon(np.array(df.lat),np.array(df.lon))
+    return np.sqrt(np.diff(X)**2,np.diff(Y)**2)
 
 def spTrav(DT,X,Y,threshold=0): # distance and speed from time (DT), lat, and lon with option for a threshold speed
     dist = distLatLon(X,Y)
@@ -88,7 +100,7 @@ def prePare(filename, convertToMin: bool = True): # prepare BIP data as per requ
     return df
 
 def A1inv(x):
-    if ((0 <= x) * (x < 0.53)):
+    if ((0 <= x) & (x < 0.53)):
         return 2 * x + pow(x,3) + (5 * pow(x,5))/6
     else:
         if (x < 0.85):
@@ -215,26 +227,25 @@ max_like = np.nan
 answ_best = np.nan
 # then perform MLE for initial headings ranging from -3 to 3
 hd_try = 3
-for x in range(len(windows)):
-    for id_hd in range(-hd_try,hd_try):
-        # perform optimisation routine
-        answ,yoko,tate = windOptim(id_hd,df.track_speed[windows[x]],df.track_direction[windows[x]],constv)
-        if (not np.isnan(tate)) & (answ.status==0):
-            # calculate speeds and headings
-            nr,nd = headSpdDir(df.track_speed[windows[x]],df.track_direction[windows[x]],answ)
-            # GOF tests
-            if np.isnan(max_like) & (np.prod(GOFtests(df.track_direction[windows[x]],nr,nd,answ)) == 1):
-                max_like = answ.fun
-                answ_best = answ
-                print(x)
-            if (answ.fun > max_like) & (np.prod(GOFtests(df.track_direction[windows[x]],nr,nd,answ)) == 1):
-                max_like = answ.fun
-                answ_best = answ
-            else:
-                answ_best = np.nan
-
+for id_hd in range(-hd_try,hd_try):
+    # perform optimisation routine
+    answ,yoko,tate = windOptim(id_hd,rrow,drow,constv)
+    if (not np.isnan(tate)) & (answ.status==0):
+        # calculate speeds and headings
+        nr,nd = headSpdDir(rrow,drow,answ)
+        # GOF tests
+        if np.isnan(max_like) & (np.prod(GOFtests(drow,nr,nd,answ)) == 1):
+            max_like = answ.fun
+            answ_best = answ
+            print(x)
+        if (answ.fun > max_like) & (np.prod(GOFtests(drow,nr,nd,answ)) == 1):
+            max_like = answ.fun
+            answ_best = answ
+        else:
+            answ_best = np.nan
+#5.692397 31.284772 -5.470312 -6.131341 11.298614
 #[range(750, 801), range(751, 802), range(752, 803), range(753, 804), range(754, 805), range(755, 806), range(756, 807), range(757, 808), range(758, 809), range(759, 810)]
-
+pars=[5.698634,31.316981,-5.485192,-6.125966,11.301431]
 
 nr,nd = headSpdDir(df.track_speed[windows[0]],df.track_direction[windows[0]],testAns)
 GOFtests(df.track_direction[windows[0]],nr,nd,testAns)
@@ -304,7 +315,7 @@ cond3 = (nrp.pvalue > 0.05) * (ndp.pvalue > 0.05) * (cnrnd.pvalue > 0.05)
 
 
 RDat = pd.read_csv("/Users/aran/Library/CloudStorage/GoogleDrive-a-garrod@g.ecc.u-tokyo.ac.jp/My Drive/PD/Data/TestingData/Rdata.txt")
-RDat.rename(columns = {'Unnamed: 0':'Index','Unnamed: 1':'DT','Unnamed: 2':'lat','Unnamed: 3':'lon','X':'X','Y':'Y','Unnamed: 6':'speed','Unnamed: 7':'head'},inplace=True)
+RDat.rename(columns = {'Unnamed: 0':'Index','Unnamed: 1':'DT','Unnamed: 2':'lat','Unnamed: 3':'lon','X':'X','Y':'Y','Unnamed: 6':'speed','Unnamed: 7':'head','Unnamed: 8':'distance'},inplace=True)
 RDat.DT = pd.to_datetime(RDat.DT,format="%Y-%m-%d %H:%M:%S")
 
 plt.plot(RDat.speed[0:100],df.track_speed[0:100])